7bea2f17fa131fd9a5c2854439bce907855a4dbe
JΛƬIП Mourya <102876688+JATINMOUR@users.noreply.github.com>
2026-01-07 00:17:05 +0530

    Delete backend directory

--- backend/src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitReview, getReviews } from "../controllers/reviewController.js";
-
-const router = express.Router();
-
-router.post("/submit", submitReview);
-router.get("", getReviews);
-
-export default router;
